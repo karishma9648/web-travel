badbc4006bd07f081e9843670fcaef365257bbb1
Karishma Singh <singhkarishma9656@gmail.com>
2026-03-07 13:07:16 +0530

    updated nav

--- header.php
+++ header.php
@@ -1,17 +1,17 @@
    
    <!-- Navbar -->
-   <nav id="navbar" class="navbar navbar-expand-lg fixed-top" style="height: 100px;">
+   <!-- <nav id="navbar" class="navbar navbar-expand-lg fixed-top" style="height: 100px;">
 
-     <div class="container-fluid position-relative">
+     <div class="container-fluid position-relative"> -->
 
        <!-- Logo -->
-       <a class="navbar-brand text-white logo" href="#">
+       <!-- <a class="navbar-brand text-white logo" href="#">
          <img src="image/logo.webp" alt="logo" class="logo-image">
-       </a> 
+       </a>  -->
 
         <!-- Toggle Button --> 
        
-       <button class="navbar-toggler custom-toggler border-0"
+       <!-- <button class="navbar-toggler custom-toggler border-0"
          type="button"
          data-bs-toggle="collapse"
          data-bs-target="#menu">
@@ -20,10 +20,10 @@
          <span class="bar"></span>
          <span class="bar"></span>
 
-       </button>
+       </button> -->
 
        <!-- Menu -->
-       <div class="collapse navbar-collapse justify-content-center mobile-menu" id="menu">
+       <!-- <div class="collapse navbar-collapse justify-content-center mobile-menu" id="menu">
          <ul class="navbar-nav  text-center" style="font-weight: 600; font-size: 20px; text-align: center; justify-content: center;">
 
            <li class="nav-item">
@@ -53,5 +53,54 @@
        </div>
 
      </div>
-   </nav>
+   </nav> -->
+   <nav id="navbar" class="navbar navbar-expand-lg fixed-top navbar-dark">
+  <div class="container">
+
+    <!-- Logo -->
+    <a class="navbar-brand" href="index.php">
+      <img src="image/logo.webp" alt="logo" class="logo-image" height="60">
+    </a>
+
+    <!-- Toggle Button -->
+    <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="menu" aria-expanded="true" aria-label="Toggle navigation">
+
+      <span class="navbar-toggler-icon"></span>
+
+    </button>
+
+    <!-- Menu -->
+    <div class="navbar-collapse justify-content-center collapse show" id="menu" data-bs-parent="#navbar">
+
+      <ul class="navbar-nav mx-auto text-center fw-semibold" style="font-size:20px;">
+
+        <li class="nav-item">
+          <a class="nav-link text-white" href="index.php">Home</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-white" href="about.php">About Us</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-white" href="terms.php">Terms &amp; Condition</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-white" href="blog.php">Blogs</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-white" href="contact.php">Contact Us</a>
+        </li>
+
+       
+
+      </ul>
+
+    </div>
+     <a class="btn  btn-hide" href="contact.php">Book Now</a>
+
+  </div>
+</nav>
    <!-- nav section send -->
